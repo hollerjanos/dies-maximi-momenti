235f63b5aa936fc567443370033b565957b980f2
Holler János <hollerjanika@gmail.com>
2025-04-01 19:33:19 +0200

    Adding the constants-example file
    Adding the file where are the necessary
    constants will be located.

---
+++ includes/constants-example.php
@@ -0,0 +1,20 @@
+<?php
+
+const DATABASE_HOSTNAME = "";
+const DATABASE_USERNAME = "";
+const DATABASE_PASSWORD = "";
+const DATABASE_DATABASE = "";
+
+const URL = "";
+
+const API_KEY = "";
+
+const LANGUAGE = "";
+
+const TITLE = "";
+
+const CHARSET = "";
+const DESCRIPTION = "";
+const KEYWORDS = "";
+const AUTHOR = "";
+const VIEWPORT = "";
